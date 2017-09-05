e60f1ac9b0c0d37f4d53e2c8182e8404b6fc185a
Daniel Acuña <danielacu_a@estudiantec.cr>
2017-09-05 01:07:59 -0600

    Quite el archivo que estaba jodiendo la vida

--- src/Directory.java
+++ src/Directory.java
@@ -6,7 +6,7 @@ public class Directory {
         File dir = new File("/home/dantroll/Documents/LinkedDB/"+value.toString());
 
         boolean successfull = dir.mkdir();
-        
+
         if (successfull){
             System.out.println("Se creó el puto");
         }else{
